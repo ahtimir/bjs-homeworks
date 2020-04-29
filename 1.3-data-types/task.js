ebd2c646e3bb60d078a3669e6a1b0bd8451d8754
ahtimir <ahtimir@gmail.com>
2020-04-29 17:04:28 +0300

    initial commit

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -1,9 +1,29 @@
+'use strict';
+
 function calculateTotalMortgage(percent, contribution, amount, date) {
-    // код для задачи №1 писать здесь
-    // return totalAmount;
-}
+   percent = parseFloat(percent);
+   contribution = parseFloat(contribution);
+   amount = parseFloat(amount);
+
+   if ((typeof percent != 'number') || (typeof contribution != 'number') || (typeof amount != 'number')) {
+    // error
+   };
+   
+   const todayIs = new Date();
+   let months = Math.ceil(Math.abs(new Date(date).getTime() - todayIs.getTime()) / (1000 * 3600 * 24)) / 30;
+   months = Math.floor(months);
+   const toReturn = amount - contribution;
+   const P = percent / 1200;
+   let monthlyRate = toReturn * ((P + P / ((Math.pow((1 + P), months) - 1))));
+   let mortgage = parseFloat((monthlyRate * months).toFixed(2));
+   console.log(`Общая выплата по ипотеке ${mortgage} ₽`);
+   return mortgage;  
+};
 
 function getGreeting(name) {
-    // код для задачи №2 писать здесь
-    // return greeting;
-}
+    if (name == '' || NaN || undefined || null) {
+        name = 'Аноним';
+    };
+    console.log(`Привет, мир! Меня зовут ${name}`);
+    return `Привет, мир! Меня зовут ${name}`;
+};
