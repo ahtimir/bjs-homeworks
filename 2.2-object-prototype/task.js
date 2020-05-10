8a79f4c0f261777cbc4a94a28d25c1380e1b9402
ahtimir <ahtimir@gmail.com>
2020-05-10 23:17:03 +0300

    initial commit

--- 2.2-object-prototype/task.js
+++ 2.2-object-prototype/task.js
@@ -1,14 +1,46 @@
+'use strict'
+
 function getAnimalSound(animal) {
-    // код для задачи №1 писать здесь
-    // return animalSound;
-}
+    if (animal === undefined) {
+        return null;
+    };
+
+    const sound = animal.sound;
+
+    if (animal != undefined) {
+        return sound;
+    };
+};
 
 function getAverageMark(marks) {
-    // код для задачи №2 писать здесь
-    // return averageMark
-}
+    let marksSum = 0,
+        average,
+        roundedAverage;
+    
+    if (marks.length === 0) {
+        return roundedAverage = 0;
+    };
+
+    for (let i = 0; i < marks.length; i++) {
+        marksSum += parseInt(marks[i]);
+    };
+
+    average = marksSum / marks.length
+    roundedAverage = Math.round(average);
+    return roundedAverage;
+};
 
 function checkBirthday(birthday) {
-    // код для задачи №3 писать здесь
-    // return verdict
-}
+    let now = Date.now();
+
+    birthday = new Date(birthday);
+
+    let diff = now - (birthday),
+        age = diff / 31557600000;
+
+    if (age >= 18) {
+        return true;
+    } else if (age < 18) {
+        return false;
+    };
+};
